a96420294010bb8c302c473f1ab359f731bb9e7b
Shaziah Gafur <shaziah_g7@hotmail.com>
2020-02-16 10:39:19 -0500

    VisionAI detects changes before and after

--- VisionAIScripts.py
+++ VisionAIScripts.py
@@ -5,22 +5,57 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
+#Perform VisionAI Image Recognition on Image BEFORE
+
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.abspath('images/vegetables.jpg')
+file_nameB = os.path.abspath('images/vegetables_before.jpg')
 
 # Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
+with io.open(file_nameB, 'rb') as image_file:
     content = image_file.read()
 
 image = types.Image(content=content)
 
 # Performs label detection on the image file
 response = client.label_detection(image=image)
-labels = response.label_annotations
+itemsBefore = response.label_annotations
 
-print('Labels:')
-for label in labels:
+#Repeat process for Image AFTER
+# The name of the image file to annotate
+file_nameA = os.path.abspath('images/vegetables_after.jpg')
+
+# Loads the image into memory
+with io.open(file_nameA, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+itemsAfter = response.label_annotations
+
+missingItems = [item for item in itemsBefore if item not in itemsAfter]
+newItems = [item for item in itemsAfter if item not in itemsBefore]
+
+print('Labels Before:')
+for label in itemsBefore:
+    print(label.description)
+
+print('\n')
+print('Labels After:')
+for label in itemsAfter:
+    print(label.description)
+
+print('\n')
+
+print('Items Missing:')
+for label in missingItems:
+    print(label.description)
+
+print('\n')
+print('Items Newly Added:')
+for label in newItems:
     print(label.description)
